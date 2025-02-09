349a61574495798deccade829fe003f60703d2fd
Никита <153456515+Lairont@users.noreply.github.com>
2025-02-10 01:14:44 +0200

    Add files via upload

---
+++ ConsoleApp9/Calculate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Calculate
+    {
+        public double num1;
+        public double num2;
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Calculate() { }
+        /// <summary>
+        /// Конструктор с одним параметром
+        /// </summary>
+        /// <param name="num1"></param>
+        public Calculate(double num1)
+        {
+            this.num1 = num1;
+        }
+        /// <summary>
+        /// Конструктор с двумя параметрами
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        public Calculate(double num1, double num2) : this(num1)
+        {
+            this.num2 = num2;
+        }
+        /// <summary>
+        /// Метод формирования строки с информацией об обьекте
+        /// </summary>
+        public void Info()
+        {
+            Console.WriteLine($"Первое число: {num1}, Второе число: {num2}");
+        }
+        /// <summary>
+        /// Метод обработки значения полей
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Результат: {Math.Sqrt(num1 * num2):F1}";
+        }
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calc1 = new Calculate();
+            calc1.num1 = 14.2;
+            calc1.num2 = 27.5;
+            calc1.Info();
+            Console.WriteLine($"{calc1.ToString()}");
+            Calculate calc2 = new Calculate(15.4,31.1);
+            calc2.Info();
+            Console.WriteLine($"{calc2.ToString()}");
+            Console.Write("Введите первое число: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double num2 = double.Parse(Console.ReadLine());
+            Calculate calc3 = new Calculate(num1, num2);
+            Console.WriteLine($"{calc3.ToString()}");
+        }
+    }
+}
